faeb4d5f2220a7832e1720cab714bc7e8574dc42
Mathieu Comandon <strycore@gmail.com>
2015-03-26 15:56:05 +0100

    Merge changes from baamenabar/jQuery-File-Upload-and-Crop

--- js/main.js
+++ js/main.js
@@ -11,7 +11,8 @@
 
 /*jslint nomen: true, unparam: true, regexp: true */
 /*global $, window, document */
-
+var $activeImage;
+var cropCoordinates={};
 $(function () {
     'use strict';
 
@@ -75,4 +76,181 @@ $(function () {
         });
     }
 
+    /***************** added by Agustín Amenabar *******************************/
+     $('#modal-gallery').on('displayed', function () {
+        var modalData = $(this).data('modal');
+        // modalData.$links is the list of (filtered) element nodes as jQuery object
+        // modalData.img is the img (or canvas) element for the loaded image
+        // modalData.options.index is the index of the current link
+        initZClip();
+        $('#urlImage').css('vertical-align','top');
+        $activeImage = $(modalData.img);
+        $('#urlImage').val($activeImage.attr('src'));
+
+        $('#inWidthCrop').val($('#croppingModal').attr('data-width'));
+        $('#inHeightCrop').val($('#croppingModal').attr('data-height'));
+        
+        $('#startCrop').click(function(eve){
+            eve.preventDefault();
+            var $cm = $('#croppingModal');
+            var cssProperties = Array('margin-left','margin-top','width');
+            for (var i = cssProperties.length - 1; i >= 0; i--) {
+                $cm.css(cssProperties[i],$('#modal-gallery').css(cssProperties[i]));
+            };
+             $cm.find('.modal-body').css('max-height','none');
+            $('#croppingModal').modal('show').find('.close, .closeModal').click(function(eve){
+                eve.preventDefault();
+                $('#croppingModal').modal('hide');
+            });
+            $('#modal-gallery').modal('hide');
+            
+            var picWidth = $activeImage.width();
+            var picHeight = $activeImage.height();
+            if (!picWidth) return;
+            $('#canvasToCrop').attr('width',picWidth);
+            $('#canvasToCrop').attr('height',picHeight);
+
+            var canContext = $('#canvasToCrop')[0].getContext("2d");
+            canContext.drawImage($activeImage[0],0,0,picWidth,picHeight);
+
+            var jcOptions = {};
+            if($('#inWidthCrop').val() && $('#inHeightCrop').val()){
+                jcOptions.aspectRatio=$('#inWidthCrop').val() / $('#inHeightCrop').val();
+                $('#croppingModal').find('h3 .dimentions').text('to '+$('#inWidthCrop').val() + ' x ' + $('#inHeightCrop').val() + ' px');
+            }
+
+            cropCoordinates.source = {
+                width:picWidth,
+                height:picHeight,
+                endWidth:$('#inWidthCrop').val(),
+                endHeight:$('#inHeightCrop').val(),
+                file:$activeImage.attr('src')
+            };
+            jcOptions.onSelect = function(c){
+                cropCoordinates.c=c;
+            };
+
+            $('#canvasToCrop').Jcrop(jcOptions);
+        });
+        $('#opCrop').find('button[type=reset]').click(function(){
+            enableStartResize(false);
+        });
+        $('#inWidthCrop').change(function(){
+            if(!$(this).val() || $(this).val()=='0'){
+                enableStartResize(false);
+                $('#inHeightCrop').val('');
+                return;
+            }else{
+                enableStartResize(true);
+            }
+        });
+        $('#inHeightCrop').change(function(){
+            if(!$(this).val() || $(this).val()=='0'){
+                enableStartResize(false);
+                $('#inWidthCrop').val('');
+                return;
+            }else{
+                enableStartResize(true);
+            }
+        });
+    });
+    $('#btnDoCrop').click(function(eve){
+        eve.preventDefault();
+        $.post('server/php/image_crop_and_size.php',cropCoordinates,afterCropping)
+    });
+    $('#startResize').click(function(){
+        var noSize=true;
+        var resizeData={ file:$activeImage.attr('src') };
+        if($('#inWidthCrop').val() && $('#inWidthCrop').val() != '0'){
+            resizeData.width=$('#inWidthCrop').val();
+            noSize=false;
+        }
+        if($('#inHeightCrop').val() && $('#inHeightCrop').val() != '0'){
+            resizeData.height=$('#inHeightCrop').val();
+            noSize=false;
+        }
+        if(noSize)return;//there's no width nor height defined to do the resize.
+        $('#startCrop, #startResize, #inWidthCrop, #inHeightCrop').attr('disabled','disabled');
+        $.post('server/php/image_crop_and_size.php',resizeData,afterResize);
+        
+    });
 });
+
+function afterCropping(data,textStatus,jqXHR){
+    $('#croppingModal').modal('hide');
+    $('tbody.files').find('tr').remove();
+    loadExistingFiles();
+}
+
+function afterResize(data,textStatus,jqXHR){
+    $('#modal-gallery').modal('hide');
+    $('#startCrop, #startResize, #inWidthCrop, #inHeightCrop').removeAttr('disabled');
+    $('tbody.files').find('tr').remove();
+    loadExistingFiles();
+}
+
+var zclipInitialized = false;
+function initZClip(){
+    if(zclipInitialized)return;
+    $('a.modal-copy').zclip({
+            path:'js/ZeroClipboard.swf',
+            copy:function(){return $('#urlImage').val();}
+        });
+    zclipInitialized=true;
+}
+
+function enableStartResize(activar){
+    $targ=$('#startResize');
+    if(activar){
+        $targ.removeAttr('disabled');
+    }else{
+        $targ.attr('disabled','disabled');
+    }
+    
+}
+
+/*
+ * Special event for image load events
+ * Needed because some browsers does not trigger the event on cached images.
+
+ * MIT License
+ * Paul Irish     | @paul_irish | www.paulirish.com
+ * Andree Hansson | @peolanha   | www.andreehansson.se
+ * 2010.
+ *
+ * Usage:
+ * $(images).bind('load', function (e) {
+ *   // Do stuff on load
+ * });
+ * 
+ * Note that you can bind the 'error' event on data uri images, this will trigger when
+ * data uri images isn't supported.
+ * 
+ * Tested in:
+ * FF 3+
+ * IE 6-8
+ * Chromium 5-6
+ * Opera 9-10
+ */
+(function ($) {
+    $.event.special.load = {
+        add: function (hollaback) {
+            if ( this.nodeType === 1 && this.tagName.toLowerCase() === 'img' && this.src !== '' ) {
+                // Image is already complete, fire the hollaback (fixes browser issues were cached
+                // images isn't triggering the load event)
+                if ( this.complete || this.readyState === 4 ) {
+                    hollaback.handler.apply(this);
+                }
+
+                // Check if data URI images is supported, fire 'error' event if not
+                else if ( this.readyState === 'uninitialized' && this.src.indexOf('data:') === 0 ) {
+                    $(this).trigger('error');
+                }
+
+                else {
+                    $(this).bind('load', hollaback.handler);
+                }
+            }
+        }
+    };
+}(jQuery));
